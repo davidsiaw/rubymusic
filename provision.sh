c37c7e4f0cf09ff8069ec0bfbb9b1c21c5b4e834
David Siaw <davidsiaw@gmail.com>
2018-04-23 17:27:37 +0900

    new version using node opus. hopefully faster

--- provision.sh
+++ provision.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-apk add --update --no-cache ffmpeg build-base git
-cd /srv && npm install
+apk add --update --no-cache ffmpeg autoconf automake make libtool python build-base git
+#git clone https://github.com/Rantanen/node-opus /srv/node-opus
+#cd /srv/node-opus
+#cd deps/opus
+#autoreconf --install
+#./configure --enable-static --disable-shared --with-pic
+#make install
 
-apk del build-base linux-headers pcre-dev openssl-dev
+npm install -g node-gyp@3.6.2
+#cd /srv/node-opus
+#npm install --unsafe-perm --verbose -g
+
+cd /srv && npm install node-opus@0.2.7 && npm install
+
+apk del autoconf automake make libtool git build-base linux-headers pcre-dev openssl-dev
 rm -rf /var/cache/apk/*
